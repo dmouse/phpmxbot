fe216ab6ea74c424cefaf6fa639b1ba24634a4e5
David Flores <david@fakku.net>
2020-11-06 02:23:43 -0600

    build using container

--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use BotMan\BotMan\BotManFactory;
-use BotMan\BotMan\Drivers\DriverManager;
-use BotMan\Drivers\Slack\SlackDriver;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use BotMan\BotMan\BotMan;
 use PhpMx\Router;
 
-DriverManager::loadDriver(SlackDriver::class);
+require 'vendor/autoload.php';
 
-$config = parse_ini_file(__DIR__ . '/../config.ini', true);
-$botman = BotManFactory::create($config);
+$containerBuilder = new ContainerBuilder();
+$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/config'));
+$loader->load('services.yaml');
+$containerBuilder->compile(true);
 
-Router::route($botman);
+$botman = $containerBuilder->get(BotMan::class);
+$router = $containerBuilder->get(Router::class);
+$router->mount();
 
 $botman->listen();
