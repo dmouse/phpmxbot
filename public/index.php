0aec9cf1b85c24ed419f67c87f1f528574c3ea28
David Flores <david@fakku.net>
2020-11-06 03:14:40 -0600

    load Slack Driver

--- public/index.php
+++ public/index.php
@@ -3,18 +3,27 @@
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Slack\SlackDriver;
+use Symfony\Component\Dotenv\Dotenv;
 use BotMan\BotMan\BotMan;
 use PhpMx\Router;
 
 require 'vendor/autoload.php';
 
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
+
 $containerBuilder = new ContainerBuilder();
-$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/config'));
+$loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__ . '/../config'));
 $loader->load('services.yaml');
 $containerBuilder->compile(true);
 
 $botman = $containerBuilder->get(BotMan::class);
 $router = $containerBuilder->get(Router::class);
+
+DriverManager::loadDriver(SlackDriver::class);
+$botman->loadDriver('Slack');
 $router->mount();
 
 $botman->listen();
